db08ccc6cc380093fc7a2e8ae5728f88e18f1950
innocentsaintguy-create <innocentsaintguy@gmail.com>
2026-07-25 02:31:04 +0100

    Add entry, stoploss, and takeprofit elements

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ let randomSignal = signals[Math.floor(Math.random() * signals.length)];
 
 trend.innerHTML = randomTrend;
 signal.innerHTML = randomSignal;
+let entry = document.getElementById("entry");
+let stoploss = document.getElementById("stoploss");
+let takeprofit = document.getElementById("takeprofit");
+
+entry.innerHTML = "4125.00";
+stoploss.innerHTML = "4115.00";
+takeprofit.innerHTML = "4145.00";
